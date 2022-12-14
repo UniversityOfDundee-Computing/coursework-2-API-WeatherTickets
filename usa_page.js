4edc1b82a23919e94b9d62a55fa703572fbc6ab9
cipher <2501402@dundee.ac.uk>
2022-12-14 01:06:17 +0000

    added comments

--- usa_page.js
+++ usa_page.js
@@ -1,6 +1,5 @@
 window.onload = function () {
 
-
     function createNode(element) {
         return document.createElement(element);
     }
@@ -11,8 +10,9 @@ window.onload = function () {
 
     const eventContainer = document.getElementById('events');
 
+    //ticketmaster API
     url = 'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&page=0&size=40&apikey=fqaqhbcCOEoIvdAxAwfBOgTmocXJowJI';
-
+    //open-meteo API
     curl = 'https://api.open-meteo.com/v1/forecast?latitude=-90.0&longitude=35.13&hourly=temperature_2m&current_weather=true';
 
     fetch(curl)
@@ -24,8 +24,6 @@ window.onload = function () {
 
             console.log(error);
         });
-
-
 
     fetch(url)
         .then((resp) => resp.json())
@@ -40,90 +38,70 @@ window.onload = function () {
                 var p = createNode('p');
                 var a = createNode('a');
 
-                //get the longitude and the latitude and round the values
+                //get the longitude and the latitude values
                 var longitude = events._embedded.venues[0].location.longitude;
-                var longitude = Math.round(longitude);
-               
                 var latitude = events._embedded.venues[0].location.latitude;
-                var latitude = Math.round(latitude);
-            
+              
+                console.log(longitude, latitude);
 
                 fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&' + 'longitude=' + longitude + '&hourly=temperature_2m&current_weather=true')
                     .then((resp) => resp.json())
                     .then(function (data) {
-                       
-                       
+
                         console.log(data.current_weather.temperature);
 
                         var p1 = createNode('p');
 
-                        p.innerHTML = data.current_weather.temperature;
+                        //display the temparature from the latitude and longitude
+                        p.innerHTML = "Current Temperature: " + data.current_weather.temperature + "℃";
                         p.classList.add("card-text");
                         append(cardBody, p);
                     })
                     .catch(function (error) {
-    
                         console.log(error);
                     });
 
+                //get the event name
+                h5.innerHTML = events.name;
+                //get the city of venue of event
+                h6.innerHTML = events._embedded.venues[0].city.name;
+                //get the type of event
+                p.innerHTML = events.classifications[0].genre.name;
+                //get an image for the event
+                img.src = events.images[0].url;
+                //get the link for the tickets
+                a.href = events.url;
 
-                  
-                  
+                var column = createNode('div');
+                column.classList.add("col-3");
+                var card = createNode('div');
+                var cardBody = createNode('div');
+                cardBody.classList.add("card-body")
 
-                        //get the event name
-                        h5.innerHTML = events.name;
-                        //get the city of venue of event
-                        h6.innerHTML = events._embedded.venues[0].city.name;
-                        //get the type of event
-                        p.innerHTML = events.classifications[0].genre.name;
-                        //get an image for the event
-                        img.src = events.images[0].url;
-                        //get the link for the tickets
-                        a.href = events.url;
+                card.classList.add("card");
+                card.classList.add("mt-3");
+                img.classList.add("card-img-top");
+                h5.classList.add("card-title");
+                h6.classList.add("card-title");
+                p.classList.add("card-text");
+                a.innerHTML = "Get Tickets";
+                a.classList.add("btn")
+                a.classList.add("btn-danger")
 
-                        var column = createNode('div');
-                        column.classList.add("col-3");
-                        var card = createNode('div');
-                        var cardBody = createNode('div');
-                        cardBody.classList.add("card-body")
+                append(card, img);
+                append(cardBody, h5);
+                append(cardBody, h6);
+                append(cardBody, p);
+                append(cardBody, a);
 
-                        card.classList.add("card");
-                        card.classList.add("mt-3");
-                        img.classList.add("card-img-top");
-                        h5.classList.add("card-title");
-                        h6.classList.add("card-title");
-                        p.classList.add("card-text");
-                        a.innerHTML = "Get Tickets";
-                        a.classList.add("btn")
-                        a.classList.add("btn-danger")
+                append(card, cardBody);
+                append(column, card);
 
-                        append(card, img);
-                        append(cardBody, h5);
-                        append(cardBody, h6);
-                        append(cardBody, p);
-                        append(cardBody, a);
-
-
-                        append(card, cardBody);
-                        append(column, card);
-
-
-                        append(eventContainer, column);
-                    }
-
-
-
-
-                    )
+                append(eventContainer, column);
             })
-                .catch(function (error) {
+        })
+        .catch(function (error) {
 
-                    console.log(error);
-                });
-
-
-
-
-
-
-        }
+            console.log(error);
+        });
+}
