238c857dff7a7465ffd5bfe9fa5ab0c75879483b
alexander gordon <alexandergordon200@gmail.com>
2022-12-14 03:57:38 +0000

    major changed to the java and syle

--- canada/javaScr.js
+++ canada/javaScr.js
@@ -8,198 +8,137 @@ function createNode(element) {
   
   
   
-//const movieContainer = document.getElementById("Movie");
-//const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=4bd6c391';
+function FetchCycle(IDTAG , classifcName   ) //example of one of the name being entered ClassifcName would be '&classificationName=festivals&' the & are needed
+{                                            //to ad muiltiply just add another search at the end of the inittial '&classificationName=festivals&size=19&'
+  const collection = document.getElementById(IDTAG);
+  const url = 'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=CA' + classifcName + 'size=20&apikey=fqaqhbcCOEoIvdAxAwfBOgTmocXJowJI';
+   
+  fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data)
+    {
   
+  console.log(data);
   
-
+      var IDTAG = data._embedded.events;
+      return IDTAG.map(function(IDTAG){
   
+           
+          
+          var card = createNode('div');
+          var img = createNode('img');
+          var h5 = createNode('h5');
+          var imgOverlay = createNode('div');
+          var p = createNode('p');
+          var column = createNode("div");
+          var footer = createNode("div")
+          
   
+          img.classList.add("card-img"); 
+          img.classList.add("rounded-3"); 
   
+          card.classList.add('card');  
+          card.classList.add('text-bg-dark');
+          card.classList.add('mt-4');
   
-//const numberOfUsers = 10;
-const SportCollection = document.getElementById('Sports');
-const urlSports = 'https://api.seatgeek.com/2/events?type=NBA&client_id=MzA5MTQ0NDV8MTY3MDY5NTgzMS4zMzk0NzQy';
-
-fetch(urlSports)
-  .then((resp) => resp.json())
-  .then(function(data)
-  {
-
-console.log(data);
-
-    var Sports = data.events;
-    return Sports.map(function(Sports){
-
+          h5.classList.add("card-title");
+          h5.classList.add("text-light");
+          h5.classList.add("text-wrap");
+          h5.classList.add("bg-dark");
+          h5.classList.add("rounded");
+          h5.classList.add("py-1");
+          h5.classList.add("px-2");
+  
+          footer.classList.add("card-footer");
+          footer.classList.add("bg-warning");
+          
+  
+          imgOverlay.classList.add("card-img-overlay");
+  
+          column.classList.add("col-xxl-3");
+          column.classList.add("col-xl-4");
+          column.classList.add("col-lg-6");
+          column.classList.add("col-md-6");
+          column.classList.add("col-sm-12");
+          
+          p.classList.add('card-text');
+          p.classList.add('text-primary');
+          
+          
+          var counter = 0 ;
+          while (true){
+            widthSport = img.src = IDTAG.images[counter].width;
+            heightSport = img.src = IDTAG.images[counter].height;
+  
+            if (widthSport == 1024 && heightSport == 683){img.src = IDTAG.images[counter].url; break;}
+  
+            counter++;
+  
+          }
+          
+           
+           
+          footer.innerHTML =  ("Date:&nbsp;" +(IDTAG.dates.start.localDate).toString())  + "<br>" + ("starts at:" + (IDTAG.dates.start.localTime).toString()) ;
+  
+          
          
-        
-        var card = createNode('div');
-        var img = createNode('img');
-        var h5 = createNode('h5');
-        var imgOverlay = createNode('div');
-        var p = createNode('p');
-        var column = createNode("div");
-        var footer = createNode("div")
-        
-
-        img.classList.add("card-img"); 
-        img.classList.add("rounded-3"); 
-
-        card.classList.add('card');  
-        card.classList.add('text-bg-dark');
-        card.classList.add('mt-4');
-
-        h5.classList.add("card-title");
-        h5.classList.add("text-light");
-        h5.classList.add("text-wrap");
-        h5.classList.add("bg-dark");
-        h5.classList.add("rounded");
-        h5.classList.add("py-1");
-        h5.classList.add("px-2");
-
-        footer.classList.add("card-footer");
-        footer.classList.add("bg-warning");
-        
-
-        imgOverlay.classList.add("card-img-overlay");
-
-        column.classList.add("col-xxl-4");
-        column.classList.add("col-xl-4");
-        column.classList.add("col-lg-6");
-        column.classList.add("col-md-6");
-        column.classList.add("col-sm-12");
-        
-        p.classList.add('card-text');
-        p.classList.add('text-primary');
-        
-        
-        
-         
-        var split = Sports.datetime_local.split('T');
-        footer.innerHTML =  split[0] ;
-        
-        img.src = Sports.performers[0].image;
-
-        h5.innerHTML = Sports.title;
-        
-        p.innerHTML = " ";
-
-
-        
-        
-
-        append(card , img);
-        append(imgOverlay,h5);
-        append(imgOverlay, p);
-        append(card, imgOverlay);
-        append(card, footer);
-        
-        append(column, card);
-        append(SportCollection, column);
-
-
-    })
-
-})
-.catch(function(error){
-
-  console.log(error);
-});
-
-
-
-const MusicCollection = document.getElementById('Music');
-const urlMusic = 'https://api.seatgeek.com/2/events?type=concert&client_id=MzA5MTQ0NDV8MTY3MDY5NTgzMS4zMzk0NzQy';
-
-fetch(urlMusic)
-  .then((resp) => resp.json())
-  .then(function(data)
-  {
-
-console.log(data);
-
-    var Music = data.events;
-    return Music.map(function(Music){
-
-         
-        
-        var card = createNode('div');
-        var img = createNode('img');
-        var h5 = createNode('h5');
-        var imgOverlay = createNode('div');
-        var p = createNode('p');
-        var column = createNode("div");
-        var footer = createNode("div")
-        
-
-        img.classList.add("card-img"); 
-        img.classList.add("rounded-3"); 
-
-        card.classList.add('card');  
-        card.classList.add('text-bg-dark');
-        card.classList.add('mt-4');
-
-        h5.classList.add("card-title");
-        h5.classList.add("text-light");
-        h5.classList.add("text-wrap");
-        h5.classList.add("bg-dark");
-        h5.classList.add("rounded");
-        h5.classList.add("py-1");
-        h5.classList.add("px-2");
-
-        footer.classList.add("card-footer");
-        footer.classList.add("bg-warning");
-        
-
-        imgOverlay.classList.add("card-img-overlay");
-
-        column.classList.add("col-xxl-4");
-        column.classList.add("col-xl-4");
-        column.classList.add("col-lg-6");
-        column.classList.add("col-md-6");
-        column.classList.add("col-sm-12");
-        
-        p.classList.add('card-text');
-        p.classList.add('text-primary');
-        
-        
-        
-         
-        var split = Music.datetime_local.split('T');
-        footer.innerHTML =  split[0] ;
-        
-        img.src = Music.performers[0].image;
-
-        h5.innerHTML = Music.title;
-        
-        p.innerHTML = " ";
-
-
-        
-        
-
-        append(card , img);
-        append(imgOverlay,h5);
-        append(imgOverlay, p);
-        append(card, imgOverlay);
-        append(card, footer);
-        
-        append(column, card);
-        append(MusicCollection, column);
-
-
-    })
-
-})
-.catch(function(error){
-
-  console.log(error);
-});
+  
+          h5.innerHTML = IDTAG.name;
+          
+          p.innerHTML = " ";
+  
+  
+          
+          
+  
+          append(card , img);
+          append(imgOverlay,h5);
+          append(imgOverlay, p);
+          append(card, imgOverlay);
+          append(card, footer);
+          
+          append(column, card);
+          append(collection, column);
+  
+  
+      })
+  
+  })
+  .catch(function(error){
+  
+    console.log(error);
+  });
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+}
+
+  
+  
+FetchCycle('Sports' , '&classificationName=festivals&' );
+FetchCycle('Hockey' , '&classificationName=hockey&' );
+FetchCycle('Rave' , '&classificationName=football&' );
+ 
+
+
+
+
+
+
+/** 
 const RaveCollection = document.getElementById('Rave');
 const urlRave = 'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=CA&city=Toronto&apikey=fqaqhbcCOEoIvdAxAwfBOgTmocXJowJI';
 
@@ -256,10 +195,19 @@ console.log(data);
         
         
         
-        img.src = Rave.images[0].url;
+        var counterRave = 0 ;
+        while (true){
+          widthSport = img.src = Rave.images[counterRave].width;
+          heightSport = img.src = Rave.images[counterRave].height;
+
+          if (widthSport == 1024 && heightSport == 683){img.src = Rave.images[counterRave].url; break;}
+
+          counterRave++;
+
+        }
 
          
-        footer.innerHTML =  Rave.dates.start.localDate ;
+        footer.innerHTML =  "Date:&nbsp;" +(Rave.dates.start.localDate).toString() + "<br>" + (Rave.dates.start.localTime).toString() ;
         
        
 
@@ -289,97 +237,109 @@ console.log(data);
   console.log(error);
 });
   
-
+*/
 
 
 
 //coppie of working fetch incase of disaster
 /**
- * const SportCollection = document.getElementById('Sports');
-const urlSports = 'https://api.seatgeek.com/2/events?type=NBA&client_id=MzA5MTQ0NDV8MTY3MDY5NTgzMS4zMzk0NzQy';
+ const HockeyCollection = document.getElementById('Hockey');
+const urlHockey = 'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=CA&classificationName=hockey&apikey=fqaqhbcCOEoIvdAxAwfBOgTmocXJowJI';
 
-fetch(urlSports)
+fetch(urlHockey)
   .then((resp) => resp.json())
   .then(function(data)
   {
 
 console.log(data);
 
-    var Sports = data.events;
-    return Sports.map(function(Sports){
+    var Hockey = data._embedded.events;
+    return Hockey.map(function(Hockey){
 
          
-         
+        
+        var card = createNode('div');
         var img = createNode('img');
         var h5 = createNode('h5');
-        var p = createNode('p')
+        var imgOverlay = createNode('div');
+        var p = createNode('p');
         var column = createNode("div");
-        var card = createNode('div');
-        var cardBody = createNode('div');
-        var cardHeader = createNode('div');
+        var footer = createNode("div")
+        
 
+        img.classList.add("card-img"); 
+        img.classList.add("rounded-3"); 
 
-            
+        card.classList.add('card');  
+        card.classList.add('text-bg-dark');
+        card.classList.add('mt-4');
 
-        cardHeader.classList.add('card-header')    
-        cardBody.classList.add('card-body');
-        card.classList.add('card');
+        h5.classList.add("card-title");
+        h5.classList.add("text-light");
+        h5.classList.add("text-wrap");
+        h5.classList.add("bg-dark");
+        h5.classList.add("rounded");
+        h5.classList.add("py-1");
+        h5.classList.add("px-2");
 
-         
+        footer.classList.add("card-footer");
+        footer.classList.add("bg-warning");
+        
 
-         
+        imgOverlay.classList.add("card-img-overlay");
 
-        column.classList.add("col-xxl-4");
+        column.classList.add("col-xxl-3");
         column.classList.add("col-xl-4");
         column.classList.add("col-lg-6");
         column.classList.add("col-md-6");
         column.classList.add("col-sm-12");
-
-        img.classList.add("card-img-top");
-        h5.classList.add("card-title");
-
-        p.classList.add('card-title');
+         
         p.classList.add('card-text');
-        card.classList.add('mt-5');
-        
-         
-         
-        var split = Sports.datetime_local.split('T');
-             
-        p.innerHTML = split[1];
-        cardHeader.innerHTML = split[0];
-
-
-        img.src = Sports.performers[0].image;
-         
-        h5.innerHTML = Sports.title;
-         
-
-
+        p.classList.add('text-primary');
         
         
+        
+         
+        var counterHockey = 0 ;
+        while (true){
+          widthSport = img.src = Hockey.images[counterHockey].width;
+          heightSport = img.src = Hockey.images[counterHockey].height;
 
-        append(card, cardHeader)
+          if (widthSport == 1024 && heightSport == 683){img.src = Hockey.images[counterHockey].url; break;}
+
+          counterHockey++;
+
+        }
+
+         
+        footer.innerHTML =  "Date:&nbsp;" +(Hockey.dates.start.localDate).toString() + "<br>" + (Hockey.dates.start.localTime).toString() ;
+        
+       
+
+        h5.innerHTML = Hockey.name;
+        
+        p.innerHTML = " ";
+
+
+        
+        
+
         append(card , img);
-        append(cardBody,h5);
-        append(cardBody, p);
-         
-        append(card, cardBody);
+        append(imgOverlay,h5);
+        append(imgOverlay, p);
+        append(card, imgOverlay);
+        append(card, footer);
+        
         append(column, card);
-        append(SportCollection, column);
-         
+        append(HockeyCollection, column);
+
 
     })
 
-  })
-  .catch(function(error){
+})
+.catch(function(error){
 
-    console.log(error);
-  });
- * 
- * 
- * 
- * 
- * 
- * 
+  console.log(error);
+});
+
  */
