6192c1c8d714666ab30b63d4213bb56c33ced501
alexander gordon <alexandergordon200@gmail.com>
2022-12-15 05:16:18 +0000

    air polution api

--- canada/javaScr.js
+++ canada/javaScr.js
@@ -6,8 +6,159 @@ function createNode(element) {
     return parent.appendChild(el);
 }
   
+//https://api.waqi.info/feed/here/?token=3b21406ee283d1489a9bc3cf5ccb723fdf99c66e
+//airpolutionAPI
   
+
+
+
+
+ 
+const url = 'https://api.waqi.info/feed/London/?token=3b21406ee283d1489a9bc3cf5ccb723fdf99c66e';
+   
+  fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data)
+    {
+
+      var condition = data.data;
+
+      console.log(data.data.city.name);
   
+    console.log(data);
+    })
+   
+  .catch(function(error){
+      console.log(error);
+  });
+
+
+
+
+  function AirDataFetch(IDTAG , classifcName   ) //example of one of the name being entered ClassifcName would be '&classificationName=festivals&' the & are needed
+  {                                            //to ad muiltiply just add another search at the end of the inittial '&classificationName=festivals&size=19&'
+    const collection = document.getElementById(IDTAG);
+    const url = 'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=CA' + classifcName + 'size=20&apikey=fqaqhbcCOEoIvdAxAwfBOgTmocXJowJI';
+     
+    fetch(url)
+      .then((resp) => resp.json())
+      .then(function(data)
+      {
+    
+    console.log(data);
+    
+        var IDTAG = data._embedded.events;
+        return IDTAG.map(function(IDTAG){
+    
+             
+            
+            var card = createNode('div');
+            var img = createNode('img');
+            var h5 = createNode('h5');
+            var imgOverlay = createNode('div');
+            var p = createNode('p');
+            var column = createNode("div");
+            var footer = createNode("div");
+            var extra = createNode('h5');
+            
+            extra.classList.add('card-header')
+            extra.classList.add('extraHeight')
+  
+            img.classList.add("card-img"); 
+            img.classList.add("rounded-3"); 
+    
+            card.classList.add('card');  
+            card.classList.add('text-bg-dark');
+            card.classList.add('mt-4');
+    
+            h5.classList.add("card-title");
+            h5.classList.add("text-light");
+            h5.classList.add("text-center");
+            h5.classList.add("text-wrap");
+            h5.classList.add("bg-dark");
+            h5.classList.add("rounded");
+            h5.classList.add("py-1");
+            h5.classList.add("px-2");
+    
+            footer.classList.add("card-footer");
+            footer.classList.add("bg-warning");
+            
+            
+            
+            extra.classList.add("bg-info");
+            
+            
+            
+            
+  
+             
+  
+            imgOverlay.classList.add("card-img-overlay");
+    
+            column.classList.add("col-xxl-3");
+            column.classList.add("col-xl-4");
+            column.classList.add("col-lg-6");
+            column.classList.add("col-md-6");
+            column.classList.add("col-sm-12");
+            
+            p.classList.add('card-text');
+            p.classList.add('text-primary');
+            
+            
+            var counter = 0 ;
+            while (true){
+              widthSport = img.src = IDTAG.images[counter].width;
+              heightSport = img.src = IDTAG.images[counter].height;
+    
+              if (widthSport == 1024 && heightSport == 683){img.src = IDTAG.images[counter].url; break;}
+    
+              counter++;
+    
+            }
+            
+            
+             
+            footer.innerHTML =  ("Date:&nbsp;" +(IDTAG.dates.start.localDate).toString())  + "<br>" + ("starts at:" + (IDTAG.dates.start.localTime).toString()) ;
+    
+            h5.innerHTML = IDTAG._embedded.venues[0].city.name;
+            
+            p.innerHTML = " ";
+  
+            //if((IDTAG.name).length == )
+            
+            extra.innerHTML = IDTAG.name + (IDTAG.name).lenght;
+    
+    
+            
+            append(card , img);
+            append(imgOverlay,h5);
+            append(imgOverlay, p);
+            append(card, imgOverlay);
+            append(card, footer);
+            append(card, extra);
+            
+            append(column, card);
+            append(collection, column);
+    
+    
+        })
+    
+    })
+    .catch(function(error){
+    
+      console.log(error);
+    });
+  }
+
+
+
+
+
+
+
+
+
+
 function FetchCycle(IDTAG , classifcName   ) //example of one of the name being entered ClassifcName would be '&classificationName=festivals&' the & are needed
 {                                            //to ad muiltiply just add another search at the end of the inittial '&classificationName=festivals&size=19&'
   const collection = document.getElementById(IDTAG);
